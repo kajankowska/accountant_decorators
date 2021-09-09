0058bd99c4d627a8b048dec42d655a0b0191d9b5
kajankowska <85677666+kajankowska@users.noreply.github.com>
2021-09-09 22:48:44 +0200

    final version

---
+++ zakup.py
@@ -0,0 +1,18 @@
+from accountant_decorators import manager
+import sys
+
+action = "purchase"
+product = sys.argv[2]
+price = int(sys.argv[3])
+quantity = int(sys.argv[4])
+
+task = (action, [product, price, quantity])
+manager.history.append(task)
+
+manager.run()
+
+with open(sys.argv[1], "a", encoding="utf-8") as file:
+    file.write("\n" + action + "\n")
+    file.write(product + "\n")
+    file.write(str(price) + "\n")
+    file.write(str(quantity) + "\n")
